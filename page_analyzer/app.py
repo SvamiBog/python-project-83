bee4d2ada58f58601aa171e7226a0674893e3fd0
SvamiBog <rubanezmaxim@gmailcom>
2024-06-09 17:47:33 +0200

    update app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
