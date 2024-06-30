0ccfa7540b7064dff94ae2f5b48b13c9d3acacfc
Maksim <rubanezmaxim@gmail.com>
2024-06-30 13:24:13 +0200

    step 3

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask, render_template
+import psycopg2
+from flask import Flask, render_template, request, redirect, url_for, flash
 from dotenv import load_dotenv
+from validators import url as validate_url
+from datetime import datetime
+from urllib.parse import urlparse
 
 load_dotenv()
 
+
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 @app.route('/')
 def index():
